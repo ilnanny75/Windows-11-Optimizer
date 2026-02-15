c8502e7669d464ad382718bdac690f039f731fcc
ilnanny75 <ilnanny75@users.noreply.github.com>
2026-02-15 05:49:18 +0100

    Remove desktop.ini and files folder, add .gitignore

--- files/Avvia_ALTERNATIVO.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-:: Launcher Alternativo - Più Semplice
-
-powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "Start-Process PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -NoExit -File \"%~dp0Windows11_Optimizer.ps1\"' -Verb RunAs"
